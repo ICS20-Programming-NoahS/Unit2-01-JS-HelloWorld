5e359a5e22bc1967a1ab0eaf37375dff00b790c6
NoahSmith04 <124379601+NoahSmith04@users.noreply.github.com>
2023-03-07 02:21:12 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 by Noah Smith All rights reserved
+//
+// Created By: Noah Smith
+// Created On: March 2023
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<b>I coded this in JavaScript!"
+}
